85fa485afd4583b6ada59acf485a4a3e624a0339
SumanPoU <acharyas186@gmail.com>
2025-11-21 10:37:12 +0545

    ::fix:: prisma new update

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,34 @@
-// lib/db.ts
+// // lib/db.ts
+
+// import { PrismaClient } from "@prisma/client";
+
+// // Avoid re-creating the Prisma client during hot reloads in development
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const db =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ["warn", "error"], // Optional: add 'query' for debugging
+//   });
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = db;
+// }
+
+// export default db;
 
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-// Avoid re-creating the Prisma client during hot reloads in development
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // only the main DB
+});
+
+const adapter = new PrismaPg(pool);
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
@@ -10,7 +36,8 @@ const globalForPrisma = globalThis as unknown as {
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["warn", "error"], // Optional: add 'query' for debugging
+    adapter,
+    log: ["warn", "error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
