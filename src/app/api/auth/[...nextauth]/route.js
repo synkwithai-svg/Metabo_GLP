bc00099691c1d7b729f9fc204380b597e2952c6d
SumanPoU <acharyas186@gmail.com>
2025-12-18 09:10:22 +0545

    ::remove:: next auth

--- src/app/api/auth/[...nextauth]/route.js
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./options";
-
-const handler = (req, res) => NextAuth(req, res, authOptions);
-
-export const GET = handler;
-export const POST = handler;
