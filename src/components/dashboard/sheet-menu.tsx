fcb676022d3c4fab75c9d20476a382e62251b851
SumanPoU <acharyas186@gmail.com>
2025-12-22 06:05:52 +0545

    ::fix:: sheet

--- src/components/dashboard/sheet-menu.tsx
+++ src/components/dashboard/sheet-menu.tsx
@@ -28,7 +28,7 @@ export function SheetMenu() {
           >
             <Link href="/dashboard" className="flex items-center gap-2">
               <PanelsTopLeft className="w-6 h-6 mr-1" />
-              <SheetTitle className="font-bold text-lg">Brand</SheetTitle>
+              <SheetTitle className="font-bold text-lg">Metabo</SheetTitle>
             </Link>
           </Button>
         </SheetHeader>
